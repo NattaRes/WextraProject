dfee9a312693ca8757866810803abcf38d1bb89b
Guideus <GGWPTH@gmail.com>
2022-10-04 03:21:43 +0700

    Data Fetching

--- adminsite/ListTools.php
+++ adminsite/ListTools.php
@@ -96,7 +96,7 @@
 
           </td>
           <td class="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
-            <a href="#" class="text-gray-600 hover:text-gray-900">
+            <a href="Toolsdetails.php?toolidall=' . $row['ID_all'] . '" class="text-gray-600 hover:text-gray-900">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" style="margin-left: 30%;" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
@@ -105,7 +105,7 @@
 
           </td>
           <td class="text-sm font-medium leading-5 whitespace-no-wrap border-b border-gray-200 ">
-            <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-600 hover:text-red-800" fill="none" style="margin-left: 30%;" stroke="currentColor">
+            <a href="../adminbackend/deltool.php?' . $row['ID_all'] . '"><svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-600 hover:text-red-800" fill="none" style="margin-left: 30%;" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
               </svg></a>
 
