394a1ebf74d659f12d1d9f2613e202549d608100
Pimpakan-Faii <97038566+Pimpakan-Faii@users.noreply.github.com>
2022-11-04 17:05:19 +0700

    UI Update

--- user/Alltools.php
+++ user/Alltools.php
@@ -159,19 +159,6 @@
 		});
 	</script>
 
-	<script>
-		function inc(element) {
-			let el = document.querySelector(`[name="${element}"]`);
-			el.value = parseInt(el.value) + 1;
-		}
-
-		function dec(element) {
-			let el = document.querySelector(`[name="${element}"]`);
-			if (parseInt(el.value) > 0) {
-				el.value = parseInt(el.value) - 1;
-			}
-		}
-	</script>
 </body>
 
 </html>
