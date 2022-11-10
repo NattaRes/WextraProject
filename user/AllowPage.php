c3c2ddd3a66ae7027d7cd6b0d76ff1804f6a0ae8
Pimpakan-Faii <97038566+Pimpakan-Faii@users.noreply.github.com>
2022-11-10 20:57:49 +0700

    up ui

--- user/AllowPage.php
+++ user/AllowPage.php
@@ -246,7 +246,7 @@
                     <label class="credit-card-label" style="font-size: 18px; color: #6e6e6e; "><?php echo $uid; ?></label>
                 </div>
                 <div>
-                    <label class="credit-card-label" style="margin-left: 5%; margin-right: 22.4%; font-size: 18px; color: #6e6e6e;">คณะ : กระทรวงเวทย์มนต์</label>
+                    <label class="credit-card-label" style="margin-left: 5%; margin-right: 22.4%; font-size: 18px; color: #6e6e6e;">คณะ : สำนักวิชาศาสตร์และศิลป์ดิจิทัล</label>
                     <label class="credit-card-label" style="font-size: 18px; color: #6e6e6e;">สาขา : เทคโนโลยีดิจิทัล</label>
                 </div>
                 <div>
