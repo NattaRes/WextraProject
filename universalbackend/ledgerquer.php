4a7d211b69fbc7f20027ef4365a20c780b8ed529
Guideus <GGWPTH@gmail.com>
2023-02-12 14:22:55 +0700

    status update

--- universalbackend/ledgerquer.php
+++ universalbackend/ledgerquer.php
@@ -14,138 +14,152 @@ $s_date = $_POST["s_date"];
 $e_date = $_POST["e_date"];
 $que_desc = $_POST["que_desc"];
 
-$newe_date = date_create($s_date);
-$e_date--;
-$newe_date->modify("+$e_date day");
-$insedate = $newe_date->format('Y-m-d');
+$chexist = "SELECT * FROM queue_table WHERE 
+    que_owner_UID = '$uid' 
+    AND (queue_status = 1
+    OR queue_status = 2
+    OR queue_status = 6)";
+$reschxt = $conn->query($chexist);
+$exiscnt = mysqli_num_rows($reschxt);
 
-// echo $uid;
-// echo $approver_UID;
-// echo $s_date;
-// echo $e_date;
-// echo $que_desc;
-// echo $newe_date->format('Y-m-d');
+if ($exiscnt < 1) {
+    $newe_date = date_create($s_date);
+    $e_date--;
+    $newe_date->modify("+$e_date day");
+    $insedate = $newe_date->format('Y-m-d');
 
-$fromselect = "SELECT tool_all_ID, quantity FROM tool_cart
-    WHERE UID = '$uid'
-    AND cart_status_ID = 2";
+    // echo $uid;
+    // echo $approver_UID;
+    // echo $s_date;
+    // echo $e_date;
+    // echo $que_desc;
+    // echo $newe_date->format('Y-m-d');
 
-$resfs = $conn->query($fromselect);
+    $fromselect = "SELECT tool_all_ID, quantity FROM tool_cart
+        WHERE UID = '$uid'
+        AND cart_status_ID = 2";
 
-// $countitem = mysqli_num_rows($resfs);
+    $resfs = $conn->query($fromselect);
 
-// echo $countitem;
-// while ($row = mysqli_fetch_array($resfs)) {
-//     echo "ALL : " . print_r($row) . "</br>";
-//     echo $row["tool_all_ID"] . "</br>";
-// }
+    // $countitem = mysqli_num_rows($resfs);
 
-$insertque = "INSERT INTO queue_table
-    (que_owner_UID, approver_UID, s_date, e_date, que_desc, queue_status)
-    VALUES ('$uid', '$approver_UID', '$s_date', '$insedate', '$que_desc', 1)";
+    // echo $countitem;
+    // while ($row = mysqli_fetch_array($resfs)) {
+    //     echo "ALL : " . print_r($row) . "</br>";
+    //     echo $row["tool_all_ID"] . "</br>";
+    // }
 
-$resinsertque = $conn->query($insertque);
+    $insertque = "INSERT INTO queue_table
+        (que_owner_UID, approver_UID, s_date, e_date, que_desc, queue_status)
+        VALUES ('$uid', '$approver_UID', '$s_date', '$insedate', '$que_desc', 1)";
 
-if ($resinsertque) {
+    $resinsertque = $conn->query($insertque);
 
-    $selque = "SELECT * FROM queue_table 
-        WHERE que_owner_UID = '$uid'
-        AND s_date = '$s_date'
-        AND e_date = '$insedate'";
+    if ($resinsertque) {
 
-    $resselque = $conn->query($selque);
+        $selque = "SELECT * FROM queue_table 
+            WHERE que_owner_UID = '$uid'
+            AND s_date = '$s_date'
+            AND e_date = '$insedate'";
 
-    while ($selque = mysqli_fetch_array($resselque)) {
-        $qid = $selque["que_ID"];
-    }
+        $resselque = $conn->query($selque);
 
-    if ($resselque) {
-
-        while ($row = mysqli_fetch_array($resfs)) {
-
-            $toolidall = $row["tool_all_ID"];
-            $quantity = $row["quantity"];
-
-            for ($x = 1; $x <= $quantity; $x++) {
-                $inserteer = "INSERT INTO ledger_table 
-                    (que_ID, user_UID, tool_all_ID, tool_spec_ID, approver_UID, 
-                    ledger_s_date, ledger_e_date, ledger_desc, queue_status) 
-                    VALUES ('$qid', '$uid', '$toolidall', NULL, '$approver_UID', 
-                    '$s_date', '$insedate', '$que_desc', 1)";
-
-                $resinsertledger = $conn->query($inserteer);
-            }
+        while ($selque = mysqli_fetch_array($resselque)) {
+            $qid = $selque["que_ID"];
         }
-        if ($resinsertledger) {
 
-            $quefetch = "SELECT * FROM queue_table
-                INNER JOIN user ON queue_table.approver_UID = user.UID
-                WHERE que_ID = '$qid'";
-            $resquefet = $conn->query($quefetch);
+        if ($resselque) {
 
-            while ($querow = mysqli_fetch_array($resquefet)) {
-                $qid = $querow["que_ID"];
-                $aprmail = $querow["email"];
-                $queowner = $querow["que_owner_UID"];
-                $maildesc = $querow["que_desc"];
-            }
+            while ($row = mysqli_fetch_array($resfs)) {
 
-            $mail = new PHPMailer(true);
+                $toolidall = $row["tool_all_ID"];
+                $quantity = $row["quantity"];
 
-            try {
-                $mail->SMTPDebug = 2;
-                $mail->isSMTP();
-                $mail->Host       = 'smtp.gmail.com';
-                $mail->SMTPAuth   = true;
-                // email here
-                $mail->Username   = 'nattawutwextramailtest@gmail.com';
-                $mail->Password   = 'cnsvhhjdeoaonfjy';
-                $mail->SMTPSecure = 'tls';
-                $mail->Port       = 587;
+                for ($x = 1; $x <= $quantity; $x++) {
+                    $inserteer = "INSERT INTO ledger_table 
+                        (que_ID, user_UID, tool_all_ID, tool_spec_ID, approver_UID, 
+                        ledger_s_date, ledger_e_date, ledger_desc, queue_status) 
+                        VALUES ('$qid', '$uid', '$toolidall', NULL, '$approver_UID', 
+                        '$s_date', '$insedate', '$que_desc', 1)";
 
-                $mail->setFrom('nattawutwextramailtest@gmail.com', 'Nattawut');
-                $mail->addAddress($aprmail);
-
-                $mail->isHTML(true);
-                $mail->Subject = 'Request approval';
-                $mail->Body    = '<html>
-                        <body>
-                            <a href="http://localhost/wextraproject/user/AllowTeacher.php?queid=' . $qid . '">
-                                CLICK
-                            </a>
-                        </body>
-                    </html>';
-                $mail->AltBody = 'http://localhost/wextraproject/user/AllowTeacher.php?queid=' . $qid . '';
-                $mail->send();
-                echo "Mail has been sent successfully!";
-
-                $emptycart = "DELETE FROM tool_cart WHERE UID = '$uid'";
-                $resemptyct = $conn->query($emptycart);
-
-                if ($resemptyct) {
-
-                    echo "<script type='text/javascript'>location.href='../user/Status.php';</script>";
-                } else {
-
-                    echo "Layer 4 : " . mysqli_error($conn);
+                    $resinsertledger = $conn->query($inserteer);
                 }
-            } catch (Exception $e) {
-                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
             }
+            if ($resinsertledger) {
 
-            // echo "<script type='text/javascript'>location.href='../user/Status.php';</script>";
+                $quefetch = "SELECT * FROM queue_table
+                    INNER JOIN user ON queue_table.approver_UID = user.UID
+                    WHERE que_ID = '$qid'";
+                $resquefet = $conn->query($quefetch);
+
+                while ($querow = mysqli_fetch_array($resquefet)) {
+                    $qid = $querow["que_ID"];
+                    $aprmail = $querow["email"];
+                    $queowner = $querow["que_owner_UID"];
+                    $maildesc = $querow["que_desc"];
+                }
+
+                $mail = new PHPMailer(true);
+
+                try {
+                    $mail->SMTPDebug = 2;
+                    $mail->isSMTP();
+                    $mail->Host       = 'smtp.gmail.com';
+                    $mail->SMTPAuth   = true;
+                    // email here
+                    $mail->Username   = 'nattawutwextramailtest@gmail.com';
+                    $mail->Password   = 'cnsvhhjdeoaonfjy';
+                    $mail->SMTPSecure = 'tls';
+                    $mail->Port       = 587;
+
+                    $mail->setFrom('nattawutwextramailtest@gmail.com', 'Nattawut');
+                    $mail->addAddress($aprmail);
+
+                    $mail->isHTML(true);
+                    $mail->Subject = 'Request approval';
+                    $mail->Body    = '<html>
+                            <body>
+                                <a href="http://localhost/wextraproject/user/AllowTeacher.php?queid=' . $qid . '">
+                                    CLICK
+                                </a>
+                            </body>
+                        </html>';
+                    $mail->AltBody = 'http://localhost/wextraproject/user/AllowTeacher.php?queid=' . $qid . '';
+                    $mail->send();
+                    echo "Mail has been sent successfully!";
+
+                    $emptycart = "DELETE FROM tool_cart WHERE UID = '$uid'";
+                    $resemptyct = $conn->query($emptycart);
+
+                    if ($resemptyct) {
+
+                        echo "<script type='text/javascript'>location.href='../user/Status.php';</script>";
+                    } else {
+
+                        echo "Layer 4 : " . mysqli_error($conn);
+                    }
+                } catch (Exception $e) {
+                    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                }
+
+                // echo "<script type='text/javascript'>location.href='../user/Status.php';</script>";
+            } else {
+
+                echo "Layer 3 : " . mysqli_error($conn);
+                // echo "<script type='text/javascript'>location.href='../user/Status.php';</script>";
+            }
         } else {
 
-            echo "Layer 3 : " . mysqli_error($conn);
-            // echo "<script type='text/javascript'>location.href='../user/Status.php';</script>";
+            echo "Layer 2 : " . mysqli_error($conn);
+            // echo "<script type='text/javascript'>location.href='../user/Alltools.php';</script>";
         }
     } else {
 
-        echo "Layer 2 : " . mysqli_error($conn);
+        echo "Layer 1 : " . mysqli_error($conn);
         // echo "<script type='text/javascript'>location.href='../user/Alltools.php';</script>";
     }
 } else {
 
-    echo "Layer 1 : " . mysqli_error($conn);
-    // echo "<script type='text/javascript'>location.href='../user/Alltools.php';</script>";
+    echo "<script>alert('คุณไม่สามารถจองคิวเพิ่มเติมได้ในขณะที่ต่อคิวอยู่หรือกำลังใช้งานอยู่');</script>";
+    echo "<script type='text/javascript'>location.href='../user/Cart.php';</script>";
 }
